f82c54696ea7a2d462e0480f9d06f9f17028b490
Md Rahmat <121531757+Itsmdrahmat@users.noreply.github.com>
2023-01-14 15:28:55 +0530

    Add files via upload

---
+++ loginpage.php
@@ -0,0 +1,21 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<?php
+			include 'css/style1.css';
+		?>
+
+	</head>
+	<body>
+		<div align="center">
+			<br><br><br><br>
+			<h1>Welcome </h1><br>
+			<a href="Logout.php" class="btn">Logout</a>
+		</div>
+	</body>
+</html>
---
+++ signup.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 14, 2023 at 10:46 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `signup`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `signup`
+--
+
+CREATE TABLE `signup` (
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mobile` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `confirm_password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `signup`
+--
+
+INSERT INTO `signup` (`id`, `name`, `email`, `mobile`, `password`, `confirm_password`) VALUES
+(21, 'Md Rahmat', 'rahmatullamd16@gmail.com', '7039431492', '$2y$10$IEhg0cy8Mg4CAnOCq93Plum9ia/AkWXqqU57ebKMep4rG943trLTC', '$2y$10$YpCyDYDVNeD8MjdsTlaHfeIxABp1Sh7iuh45TXozFoW7/2StsMhAe');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `signup`
+--
+ALTER TABLE `signup`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `signup`
+--
+ALTER TABLE `signup`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
